b69b93315c38dc67f80f4ec9429b1ebcd2b8576b
Anuragyadav27 <anuragyadav290902@gmail.com>
2023-11-12 23:41:17 +0530

    Database connected & routing

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
 
 mongoose.connect(process.env.MONGODB).then(() => {
     console.log('Connected to Database');
@@ -10,7 +14,13 @@ mongoose.connect(process.env.MONGODB).then(() => {
 
 const app = express();
 
+app.use(express.json());
+
+
 app.listen(3000,() => {
     console.log('listening on port 3000!!');
     }
 )
+
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
